a5de2625bfab4c48ff61de798daa7ca1511b69cd
MAWAAW <dark1111@hotmail.fr>
2018-01-08 18:40:37 +0100

    clean code

--- src/sessions/list/index.js
+++ src/sessions/list/index.js
@@ -1,10 +1,16 @@
 import TalkService from '../../common/talk.service'
 
-export function render(talkService, idView) {
-    talkService
-        .findAllSessions()
-        .then(resp => resp.json())
-        .then(sessions => {
-            sessions.forEach(session => document.getElementById(idView).innerHTML += '<p>' + session.title + '</p>')
-        })
+
+export default class SessionList {
+    constructor(talkService) {
+        this.talkService = talkService
+    }
+    render(idView) {
+        this.talkService
+            .findAllSessions()
+            .then(resp => resp.json())
+            .then(sessions => {
+                sessions.forEach(session => document.getElementById(idView).innerHTML += '<p>' + session.title + '</p>')
+            })
+    }
 }
